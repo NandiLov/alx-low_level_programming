cc8122e33bd19041204371d567031638c6bae4e9
NandiLov <nandilamul@gmail.com>
2022-12-09 12:51:10 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - receives no arguement
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+}
