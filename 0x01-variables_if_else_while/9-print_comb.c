4360db263170c9ed28a15398b1bb1dcf2d7690c8
NandiLov <nandilamul@gmail.com>
2022-12-09 13:31:34 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,9 +7,17 @@
  */
 int main(void)
 {
-		putchar((num % 10) + '0');
-		if (num == 9)
+	int digit;
+		for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
 			continue;
+
 		putchar(',');
 		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
 }
