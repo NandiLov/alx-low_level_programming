78beb5e20061abdcf304ea049b416b6c0a1a4974
NandiLov <nandilamul@gmail.com>
2022-12-15 11:57:23 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* int mul - multiply integers a and b
+* @a: first integer
+* @b: second integer
+* @mul: is a * b
+* Return: 0
+*/
+int mul(int a, int b)
+{
+	int a;
+	int b;
+	int mul = a * b;
+
+		{
+		printf("%d * %d = %d\n", a, b, int mul);
+		return (0);
+		}
+}
